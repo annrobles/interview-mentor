919713246016926bfe20658df8f5baa8e12f1ec7
Florece Ann Robles <annr.dev@gmail.com>
2023-06-14 23:16:41 -0400

    Added Question Model

---
+++ interview-mentor/Model/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  interview-mentor
+//
+//  Created by Ann Robles on 6/14/23.
+//
+import Foundation
+import UIKit
+
+struct Question {
+    
+    var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+    
+}
